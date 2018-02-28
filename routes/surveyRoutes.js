06dd61ebb1dbea27969915839aac26a25a36cd57
Maxwell Cady <maxwell.josef@gmail.com>
2018-02-27 19:09:26 -0600

    wrap up survey routes, backend nearly done.

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const Path = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -7,9 +10,38 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('<h1 style="text-align: center;">Thanks so much for your feedback!</h1>');
   })
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const p = new Path('/api/surveys/:surveyId/:choice');
+
+    _.chain(req.body)
+      .map(({email, url}) => { // get email and url from event object
+        const match = p.test(new URL(url).pathname); // Checks if pathname has user's email, surveyId and choice
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact() // removes undefined
+      .uniqBy('email', 'surveyId') // removes duplicates
+      .each(({ surveyId, email, choice}) => {
+        Survey.updateOne({
+          _id: surveyId,
+          recipients: {
+            $elemMatch: { email: email, responded: false } // Finds subdoc collection
+          }
+        }, {
+          $inc: { [choice]: 1 }, // increment yes or no ([choice]) by 1
+          $set: { 'recipients.$.responded': true }, // $ matches to elemMatch above
+          lastResponded: new Date()
+        }).exec(); // executes query
+      })
+      .value(); // returns new array
+
+    res.send({}); // don't leave sendgrid hanging
+  });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
@@ -20,7 +52,7 @@ module.exports = app => {
       body,
       recipients: recipients.split(',').map(email => ({ email })),
       _user: req.user.id,
-      dateSend: Date.now()
+      dateSent: Date.now()
     });
 
     const mailer = new Mailer(survey, surveyTemplate(survey));
